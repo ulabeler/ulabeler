b7b05873dd2bd0fdac50335feb648b5ea99fb813
yama <na2na.rta@gmail.com>
2021-11-26 16:16:48 +0900

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+"use strict";
+var express = require('express');
+var router = express.Router();
+/* GET users listing. */
+router.get('/', function (request, response, next) {
+    response.send('respond with a resource');
+});
+module.exports = router;
