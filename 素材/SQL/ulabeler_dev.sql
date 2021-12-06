9a6b8a391dd4cecb4e755d0ac8cae5d282170af3
na2na-p <na2na.rta@gmail.com>
2021-12-06 16:46:40 +0900

    Fix[DB]base_category.name_subcategoryのNullAbleを禁止に

--- 素材/SQL/ulabeler_dev.sql
+++ 素材/SQL/ulabeler_dev.sql
@@ -1,24 +1,24 @@
 -- MySQL Workbench Forward Engineering
--- 本番用
+-- 開発用
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema ulabeler
+-- Schema ulabeler_dev
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema ulabeler
+-- Schema ulabeler_dev
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ulabeler` DEFAULT CHARACTER SET utf8mb4 ;
-USE `ulabeler` ;
+CREATE SCHEMA IF NOT EXISTS `ulabeler_dev` DEFAULT CHARACTER SET utf8mb4 ;
+USE `ulabeler_dev` ;
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`user`
+-- Table `ulabeler_dev`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`user` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`user` (
   `id` VARCHAR(15) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
@@ -38,9 +38,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`delivery_address`
+-- Table `ulabeler_dev`.`delivery_address`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`delivery_address` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`delivery_address` (
   `user_id` VARCHAR(15) NOT NULL,
   `updated_at` DATETIME NOT NULL,
   `zip_code` CHAR(7) NOT NULL,
@@ -52,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`delivery_address` (
   PRIMARY KEY (`user_id`, `updated_at`),
   CONSTRAINT `user_id_delivery_addr`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`vendor`
+-- Table `ulabeler_dev`.`vendor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`vendor` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`vendor` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
@@ -69,9 +69,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`category_settings`
+-- Table `ulabeler_dev`.`category_settings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`category_settings` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`category_settings` (
   `name` VARCHAR(45) NOT NULL,
   `thumbnail_path` VARCHAR(255) NULL,
   `created_at` DATETIME NULL,
@@ -80,9 +80,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`base_category`
+-- Table `ulabeler_dev`.`base_category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`base_category` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`base_category` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `vendor_id` INT NOT NULL,
   `name_category` VARCHAR(45) NOT NULL,
@@ -92,21 +92,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`base_category` (
   INDEX `category_settings_name_idx` (`name_subcategory` ASC) VISIBLE,
   CONSTRAINT `vendor_id`
     FOREIGN KEY (`vendor_id`)
-    REFERENCES `ulabeler`.`vendor` (`id`)
+    REFERENCES `ulabeler_dev`.`vendor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `category_settings_name`
     FOREIGN KEY (`name_subcategory`)
-    REFERENCES `ulabeler`.`category_settings` (`name`)
+    REFERENCES `ulabeler_dev`.`category_settings` (`name`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`work`
+-- Table `ulabeler_dev`.`work`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`work` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`work` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `created_by_user_id` VARCHAR(15) NOT NULL,
   `base_category_id` INT NOT NULL,
@@ -127,21 +127,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`work` (
   UNIQUE INDEX `work_tex_path_UNIQUE` (`work_tex_path` ASC) VISIBLE,
   CONSTRAINT `user_id_work`
     FOREIGN KEY (`created_by_user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `category_id`
     FOREIGN KEY (`base_category_id`)
-    REFERENCES `ulabeler`.`base_category` (`id`)
+    REFERENCES `ulabeler_dev`.`base_category` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`purchase_history`
+-- Table `ulabeler_dev`.`purchase_history`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`purchase_history` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`purchase_history` (
   `id` VARCHAR(20) NOT NULL,
   `user_id` VARCHAR(15) NOT NULL,
   `items` JSON NOT NULL,
@@ -152,16 +152,16 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`purchase_history` (
   INDEX `user_id_Index` (`user_id` ASC) VISIBLE,
   CONSTRAINT `user_id_purchase`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`favorited_work`
+-- Table `ulabeler_dev`.`favorited_work`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_work` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`favorited_work` (
   `favorite_from` VARCHAR(15) NOT NULL,
   `favorite_to` INT NOT NULL,
   `favorited_at` DATETIME NOT NULL,
@@ -169,21 +169,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_work` (
   INDEX `F_work_ID_idx` (`favorite_to` ASC) VISIBLE,
   CONSTRAINT `user_id_fav_work`
     FOREIGN KEY (`favorite_from`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `work_id_fav_work`
     FOREIGN KEY (`favorite_to`)
-    REFERENCES `ulabeler`.`work` (`id`)
+    REFERENCES `ulabeler_dev`.`work` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`base_settings`
+-- Table `ulabeler_dev`.`base_settings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`base_settings` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`base_settings` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `object_path` VARCHAR(255) NOT NULL,
   `base_tex_path` VARCHAR(255) NOT NULL,
@@ -196,16 +196,16 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`base_settings` (
   UNIQUE INDEX `thumbnail_path_UNIQUE` (`thumbnail_path` ASC) VISIBLE,
   CONSTRAINT `T_category_id`
     FOREIGN KEY (`id`)
-    REFERENCES `ulabeler`.`base_category` (`id`)
+    REFERENCES `ulabeler_dev`.`base_category` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`stamp_settings`
+-- Table `ulabeler_dev`.`stamp_settings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`stamp_settings` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`stamp_settings` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `img_path` VARCHAR(255) NOT NULL,
@@ -216,9 +216,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`favorited_user`
+-- Table `ulabeler_dev`.`favorited_user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_user` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`favorited_user` (
   `favorite_from` VARCHAR(15) NOT NULL,
   `favorite_to` VARCHAR(15) NOT NULL,
   `favorited_at` DATETIME NOT NULL,
@@ -226,21 +226,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_user` (
   INDEX `user_id_fav_user_to_idx` (`favorite_to` ASC) VISIBLE,
   CONSTRAINT `user_id_fav_user_from`
     FOREIGN KEY (`favorite_from`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `user_id_fav_user_to`
     FOREIGN KEY (`favorite_to`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`report`
+-- Table `ulabeler_dev`.`report`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`report` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`report` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `reported_to_user_id` VARCHAR(15) NOT NULL,
   `reported_from_user_id` VARCHAR(15) NOT NULL,
@@ -252,21 +252,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`report` (
   INDEX `user_id_report_from_idx` (`reported_from_user_id` ASC) VISIBLE,
   CONSTRAINT `user_id_report_to`
     FOREIGN KEY (`reported_to_user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `user_id_report_from`
     FOREIGN KEY (`reported_from_user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`mail_confirmation`
+-- Table `ulabeler_dev`.`mail_confirmation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`mail_confirmation` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`mail_confirmation` (
   `user_id` VARCHAR(15) NOT NULL,
   `datetime_issue` DATETIME NOT NULL,
   `mailaddress_new` VARCHAR(255) NULL,
@@ -275,32 +275,32 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`mail_confirmation` (
   INDEX `F_user_ID_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `user_id_main_confim`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`password_reset`
+-- Table `ulabeler_dev`.`password_reset`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`password_reset` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`password_reset` (
   `id` VARCHAR(15) NOT NULL,
   `datetime_issue` DATETIME NOT NULL,
   `temp_password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`, `datetime_issue`),
   CONSTRAINT `user_id_pass_reset`
     FOREIGN KEY (`id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`work_used_stamps`
+-- Table `ulabeler_dev`.`work_used_stamps`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`work_used_stamps` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`work_used_stamps` (
   `work_id` INT NOT NULL,
   `stamp_settings_id` INT NOT NULL,
   `num_used` INT NULL,
@@ -308,21 +308,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`work_used_stamps` (
   INDEX `id_stamp_idx` (`stamp_settings_id` ASC) VISIBLE,
   CONSTRAINT `id_work`
     FOREIGN KEY (`work_id`)
-    REFERENCES `ulabeler`.`work` (`id`)
+    REFERENCES `ulabeler_dev`.`work` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `id_stamp`
     FOREIGN KEY (`stamp_settings_id`)
-    REFERENCES `ulabeler`.`stamp_settings` (`id`)
+    REFERENCES `ulabeler_dev`.`stamp_settings` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`administrator`
+-- Table `ulabeler_dev`.`administrator`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`administrator` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`administrator` (
   `id` VARCHAR(15) NOT NULL,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
@@ -330,9 +330,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`inquiry`
+-- Table `ulabeler_dev`.`inquiry`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`inquiry` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`inquiry` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` VARCHAR(15) NOT NULL,
   `category` TINYINT NOT NULL,
@@ -349,21 +349,21 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`inquiry` (
   INDEX `admin_id_inq_idx` (`replyed_by_user_id` ASC) VISIBLE,
   CONSTRAINT `user_id_inq`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `admin_id_inq`
     FOREIGN KEY (`replyed_by_user_id`)
-    REFERENCES `ulabeler`.`administrator` (`id`)
+    REFERENCES `ulabeler_dev`.`administrator` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`delivery_status`
+-- Table `ulabeler_dev`.`delivery_status`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`delivery_status` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`delivery_status` (
   `purchase_record_id` VARCHAR(20) NOT NULL,
   `datetime_scheduled` VARCHAR(20) NULL COMMENT '配送予定日時\n例)\n08/30　08:00-12:00',
   `current_status` VARCHAR(45) NULL,
@@ -378,16 +378,16 @@ CREATE TABLE IF NOT EXISTS `ulabeler`.`delivery_status` (
   PRIMARY KEY (`purchase_record_id`),
   CONSTRAINT `purchase_record_id`
     FOREIGN KEY (`purchase_record_id`)
-    REFERENCES `ulabeler`.`purchase_history` (`id`)
+    REFERENCES `ulabeler_dev`.`purchase_history` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`sample_images`
+-- Table `ulabeler_dev`.`sample_images`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`sample_images` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`sample_images` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `sample_images_path` VARCHAR(255) NULL,
   `added_at` DATETIME NULL,
@@ -396,37 +396,37 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`favorited_user_number`
+-- Table `ulabeler_dev`.`favorited_user_number`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_user_number` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`favorited_user_number` (
   `favorited_to_id` VARCHAR(15) NOT NULL,
   `number` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`favorited_to_id`),
   CONSTRAINT `user_id_favnum_user`
     FOREIGN KEY (`favorited_to_id`)
-    REFERENCES `ulabeler`.`user` (`id`)
+    REFERENCES `ulabeler_dev`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ulabeler`.`favorited_work_number`
+-- Table `ulabeler_dev`.`favorited_work_number`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ulabeler`.`favorited_work_number` (
+CREATE TABLE IF NOT EXISTS `ulabeler_dev`.`favorited_work_number` (
   `favorited_to_id` INT NOT NULL,
   `number` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`favorited_to_id`),
   CONSTRAINT `favorited_to_id`
     FOREIGN KEY (`favorited_to_id`)
-    REFERENCES `ulabeler`.`work` (`id`)
+    REFERENCES `ulabeler_dev`.`work` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 CREATE USER 'ulabeler' IDENTIFIED BY 'ulabeler';
 
-GRANT ALL ON `ulabeler`.* TO 'ulabeler';
+GRANT ALL ON `ulabeler_dev`.* TO 'ulabeler';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
