93076f1ace0a56e11e144350a9cfc71ee38bae17
na2na <na2na.rta@gmail.com>
2022-01-11 01:05:48 +0900

    [Update]いろいろ

--- routes/test.ts
+++ routes/test.ts
@@ -7,4 +7,8 @@ router.get('/', function(request:any, response:any, next:any) {
     response.render('testuse/index', { set_picture: set_picture });
 });
 
+router.get('/train-canvas', function(request:any, response:any, next:any) {
+    response.render('testuse/train-canvas');
+});
+
 module.exports = router;
