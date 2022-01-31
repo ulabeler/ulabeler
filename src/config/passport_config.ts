58b3b5bb2331563391a38c8d9b8ff9e6a35cf271
na2na-p <na2na@na2na.dev>
2022-02-01 04:20:18 +0900

    passport.jsの設定完了?

--- src/config/passport_config.ts
+++ src/config/passport_config.ts
@@ -1,11 +1,35 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+import bcrypt from 'bcrypt';
+import { knex } from '../app';
+import { userTable } from '../tableType_alias';
 
-// module.exports = function (app: any) {
-//     passport.use(new LocalStrategy({
-//         usernameField: "username",
-//         passwordField: "password",
-//     }, function (username:string, password:string, done:any) {
-//     }
-//     ));
-// };
+module.exports = function (app: any) {
+    passport.serializeUser(function (user: userTable, done: any) {
+        done(null, user.id);
+    });
+    passport.deserializeUser(function (id: string, done: any) {
+        knex('user').where({ id: id }).first().then((user: userTable) => {
+            done(null, user);
+        });
+    });
+    passport.use(new LocalStrategy({
+        usernameField: "username", //inputのname属性
+        passwordField: "password",
+    }, function (username: string, password: string, done: any) {
+        knex('user').where('id', username).then((result: userTable[]) => {
+            if (result.length === 0) {
+                return done(null, false, { message: 'ユーザーが存在しません' });
+            } else {
+                bcrypt.compare(password, result[0].password, function (err: any, res: boolean) {
+                    if (res) {
+                        return done(null, result[0]);
+                    } else {
+                        return done(null, false, { message: 'パスワードが違います' });
+                    }
+                });
+            }
+        });
+    }
+    ));
+};
