ae439469d523a2f9bdf5dd2ea542c195eaecc116
na2na <na2na.rta@gmail.com>
2022-02-01 01:57:09 +0900

    α版一部ファイルの追跡解除

--- src/public/javascripts/.old/slick.js
+++
@@ -1,8 +0,0 @@
-$('.animals').slick({
-    dots: true,//下の黒い点
-    variableWidth: true,
-    //autoplaySpeed: 1000,//自動スクロール速度
-    //speed: 1000,//自動スクロール速度
-    //autoplay: true,//自動スクロール
-  　//centerMode: true,//センター表示
-  });
