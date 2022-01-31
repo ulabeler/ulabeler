c118a114b1860c34a6a7d806ab335f0284c88772
na2na <na2na.rta@gmail.com>
2022-02-01 01:52:54 +0900

    破壊的変更 srcディレクトリの新設とそれに関係する多数ファイルの移動

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra');
 cp_dir();
 
 function cp_dir(){
-fse.copySync('./public', './built/public');
-fse.copySync('./views', './built/views');
-fse.copySync('./tools/data/mailText.json', './built/tools/data/mailText.json');
+fse.copySync('./src/public', './built/public');
+fse.copySync('./src/views', './built/views');
+fse.copySync('./src/tools/data/mailText.json', './built/tools/data/mailText.json');
 }
