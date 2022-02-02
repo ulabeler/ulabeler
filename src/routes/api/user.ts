e4a90dbeaab6c0083f3d2d32e8fe5780077a64c2
na2na-p <na2na@na2na.dev>
2022-02-02 16:16:09 +0900

    パスワード変更を最低限動くようにした

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -125,13 +125,13 @@ router.post('/reset_password', function (request, response) {
         const temp: password_resetTable = {
           id: results[0].id,
           datetime_issue: new Date(),
-          temp_password: bcrypt.hashSync(Math.random().toString(36).slice(-8), 10),
+          temp_password: uuidv4().replace(/-/g, ''),
           token: uuidv4().replace(/-/g, '')
         }
         //password_resetTableへ登録
         knex('password_reset').insert({
           id: temp.id,
-          temp_password: bcrypt.hashSync(temp.temp_password, 10),
+          temp_password:  bcrypt.hashSync(temp.temp_password, 10),
           datetime_issue: temp.datetime_issue,
           token: temp.token
         }).then(function () {
@@ -140,7 +140,7 @@ router.post('/reset_password', function (request, response) {
 
           const host = env === 'development' ? 'http://localhost:3001' : 'https://ulabeler.na2na.website';
 
-          const message = `<p>パスワード再設定のお知らせです。<br>仮のパスワードは以下を使用してください。<br>${temp.temp_password}<br><a href='${host}/reset_password?token=${temp.token}'>こちら</a>からパスワードを再設定してください。</p>`
+          const message = `<p>パスワード再設定のお知らせです。<br>仮のパスワードは以下を使用してください。<br>${temp.temp_password}<br><a href='${host}/reset_password?token=${temp.token}&id=${temp.id}'>こちら</a>からパスワードを再設定してください。</p>`
           sendMail("reset_password", mailaddress, message);
           response.status(201).send(true);
         }).catch(function (err: any) {
@@ -159,7 +159,7 @@ router.post('/reset_password', function (request, response) {
 
 router.post('/reset_password_attempt', function (request, response) {
   //パラメーターが足りなければ400
-  if (!request.body.token || !request.body.password || !request.body.temp_password || !request.body.id) {
+  if (!request.body.token || !request.body.new_password || !request.body.temp_password || !request.body.id) {
     response.status(400).send('Bad Request');
   } else {
     //パラメーターからidを取得
@@ -167,38 +167,49 @@ router.post('/reset_password_attempt', function (request, response) {
     //パラメーターからtokenを取得
     const token: password_resetTable['token'] = request.body.token;
     //パラメーターからパスワードを取得
-    const password: userTable['password'] = bcrypt.hashSync(request.body.password, 10);
+    const password: userTable['password'] = request.body.new_password;
     //パラメーターから仮のパスワードを取得
-    const temp_password: password_resetTable['temp_password'] = bcrypt.hashSync(request.body.temp_password, 10);
+    const temp_password: password_resetTable['temp_password'] = request.body.temp_password;
 
-    //password_resetでtokenとidが一致するものを取得
-    knex('password_reset').where('token', token).andWhere('id', id).andWhere('temp_password', temp_password).select('id').then((results: any) => {
-      if (results.length > 0) {
-        //パスワードを変更
-        knex('user').where('id', id).update({
-          password: password
-        }).then(function () {
-          //仮のパスワードを削除
-          knex('password_reset').where('id', id).del().then(function () {
-            response.status(200).send(true);
-          }).catch(function (err: any) {
-            console.log(err);
-            response.status(500).send('Internal Server Error');
+    console.log(id);
+    console.log(token);
+    console.log(password);
+    console.log(temp_password);
+
+
+      //password_resetでtokenとidが一致するものを取得
+      knex('password_reset').where('token', token).andWhere('id', id).select('*').then((results: any) => {
+        if (results.length > 0) {
+          bcrypt.compare(temp_password, results[0].temp_password, function (err: any, result: boolean) {
+            if (result) {
+              //パスワードを変更
+              knex('user').where('id', id).update({
+                password: bcrypt.hashSync(password, 10)
+              }).then(function () {
+                //password_resetを削除
+                knex('password_reset').where('id', id).del().then(function () {
+                  response.status(200).send(true);
+                }).catch(function (err: any) {
+                  console.log(err);
+                  response.status(500).send('Internal Server Error');
+                });
+              }).catch(function (err: any) {
+                console.log(err);
+                response.status(500).send('Internal Server Error');
+              });
+            } else {
+              response.status(401).send("Temp Password is wrong");
+            }
           });
-        }).catch(function (err: any) {
-          console.log(err);
-          response.status(500).send('Internal Server Error');
-        });
-      } else {
-        response.status(200).send(false);
-      }
-    }).catch(function (err: any) {
-      console.log(err);
-      response.status(500).send('Internal Server Error');
-    }
-    );
+        } else {
+          response.status(401).send("Token is wrong");
+        }
+      }).catch(function (err: any) {
+        console.log(err);
+        response.status(500).send('Internal Server Error');
+      });
   }
-})
+});
 
 //CLI専用
 //該当idのユーザーを物理削除
