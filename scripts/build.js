ae6b29984e285ab9c53e88c5477746433309921b
na2na-p <na2na@na2na.dev>
2022-02-05 02:15:35 +0900

    .

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,17 @@
-const fse = require('fs-extra');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fse = require("fs-extra");
 
-cp_dir();
-
-function cp_dir(){
-fse.copySync('./src/public', './built/public');
-fse.copySync('./src/views', './built/views');
-fse.copySync('./src/tools/data/mailText.json', './built/tools/data/mailText.json');
+const distDir = "./built";
+cpDir(distDir);
+// srcにある、コンパイルしないタイプのファイルをbuiltにコピーする
+/**
+ * @param {string} distDir
+ */
+function cpDir(distDir) {
+  fse.copySync("./src/public", `${distDir}/public`);
+  fse.copySync("./src/views", `${distDir}/views`);
+  fse.copySync(
+    "./src/tools/data/mailText.json",
+    `${distDir}/tools/data/mailText.json`
+  );
 }
