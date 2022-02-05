17aa3123b1128f7c4830e274cc7f9c9e72e2e496
na2na-p <na2na@na2na.dev>
2022-02-06 04:47:41 +0900

    ポップアップ問題の解決

--- src/public/javascripts/pop-up.ts
+++ src/public/javascripts/pop-up.ts
@@ -1,33 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// const TargetButton = document.querySelectorAll('.js-popupLogin');
-// const TargetButtonGetById = document.getElementById('js-popupLogin');
 const TargetBlock = document.querySelector('.popup');
 const blackBg = document.getElementById('js-black-bg');
+const blackBgClass = document.querySelectorAll('.js-black-bg');
 const closeBtn = document.getElementById('js-close-btn');
 const showBtn = document.getElementById('js-show-popup');
-// ボタンが押されたら、ポップアップを表示する
-/**
- * @return {void}
- */
-function popup() {
-  TargetBlock!.classList.add('is-show');
-  closePopUp(blackBg);
-  closePopUp(closeBtn);
-  closePopUp(showBtn);
-}
-
-// ポップアップを閉じる
-/**
- * @param {*} element
- * @return {void}
- */
-function closePopUp(element: any) {
-  if (!element) return;
-  element.addEventListener('click', function () {
-    // is-showを無効に
-    TargetBlock!.classList.remove('is-show');
-  });
-}
 
 // ポップアップを開く
 /**
@@ -39,7 +15,19 @@ function showPopUp(id: string) {
   const target= document.getElementById(id);
   console.log(target);
     target!.classList.add('is-show');
-    closePopUp(blackBg);
-    closePopUp(closeBtn);
-    closePopUp(showBtn);
+    // closePopUp(blackBgClass);
+    // closePopUp(closeBtn);
+    // closePopUp(showBtn);
 }
+
+// ポップアップを閉じる
+/**
+ * @param {id} string
+ * @return {void}
+ *///
+ function closePopUp(id: string) {
+  console.log(id);
+  const target= document.getElementById(id);
+  console.log(target);
+    target!.classList.remove('is-show');
+};
