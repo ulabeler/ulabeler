d8860b4742e8f28cc14f7f7638b370a6ca99125b
na2na-p <na2na@na2na.dev>
2022-02-08 03:59:32 +0900

    Update[作品情報編集ページ]遷移と入力バリデーションまで

--- src/routes/work.ts
+++ src/routes/work.ts
@@ -3,7 +3,7 @@ import express from "express";
 // import { sendMail } from "../../tools/sendmail";
 import { knex } from "../app";
 // eslint-disable-next-line camelcase
-import { workTable } from "../tools/TypeAlias/tableType_alias";
+import { workTable, base_categoryTable } from "../tools/TypeAlias/tableType_alias";
 import sideMenuList from "../tools/data/sidemenu.json";
 // import { v4 as uuidv4 } from "uuid";
 // eslint-disable-next-line new-cap
@@ -42,6 +42,34 @@ router.get("/:workId/report", function (request, response) {
         }
     } else {
         response.send("fugafuga");
+    }
+})
+
+router.get("/:workId/edit", function (request, response) {
+    if (request.user) {
+        if (request.params.workId) {
+            const workId = request.params.workId;
+            knex("work")
+                .where("id", workId)
+                .then(function (work: workTable[]) {
+                    if (work.length > 0) {
+                        // work[0]のbase_category_idに一致するname_subcategoryをbase_categoryから取得
+                        knex("base_category")
+                            .where("id", work[0].base_category_id)
+                            .then(function (base_category: base_categoryTable[]) {
+                                if (base_category.length > 0) {
+                                    response.render("work/work_detail_setting", {
+                                        baseCategory: base_category[0],
+                                        work: work[0],
+                                        side_menu: JSON.parse(JSON.stringify(sideMenuList))[`${Boolean(request.user)}`],
+                                        userInfo: request.user,
+                                    });
+                                }
+                            }
+                            )
+                    }
+                })
+        }
     }
 })
 
