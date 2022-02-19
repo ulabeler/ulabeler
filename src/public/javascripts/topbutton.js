3467c07f48e9ad448c093a77796df9409ae1be16
na2na <49822810+na2na-p@users.noreply.github.com>
2022-02-20 02:11:33 +0900

    head内とフッターのコンポーネント化

---
+++ src/public/javascripts/topbutton.js
@@ -0,0 +1,19 @@
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
