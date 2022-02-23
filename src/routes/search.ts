ec4d7a8d2a04126e37a8663dc102e4e112defe7b
na2na <49822810+na2na-p@users.noreply.github.com>
2022-02-24 03:25:18 +0900

    ユーザー単体検索(という名のリダイレクト機能)まで完了

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,16 +1,41 @@
 import express from "express";
-// import { knex } from "../app";
+import { knex } from "../app";
 // import bcrypt from 'bcrypt';
 // eslint-disable-next-line new-cap
 const router = express.Router();
 // import sideMenuList from "../tools/data/sidemenu.json";
 import { searchWordParse } from "../tools/parser";
 
+import { userTable } from "tools/TypeAlias/tableType_alias";
+
 router.get("/", (request, response) => {
   console.log(request.query.q);
   const searchQuery = searchWordParse(request.query.q as string);
   console.table(searchQuery);
-  response.status(200).send("OK");
+  if (searchQuery.length !== 0) {
+    // searchQueryに@が含まれる要素がある場合、userTableからそのユーザーのidを取得
+    if (searchQuery.length === 1 && searchQuery[0].includes("@")) {
+      response.redirect(`/creator_work/${searchQuery[0].slice(1)}`);
+      return;
+    } else {
+      for (let i = 0; i < searchQuery.length; i++) {
+        if (searchQuery[i].indexOf("@") !== -1) {
+          knex("user")
+            .select("id", "name", "icon_path", "self_introduction")
+            .where("id", searchQuery[i].slice(1))
+            .then((resultUser: userTable[]) => {
+              if (resultUser.length !== 0) {
+                response.status(200).json(resultUser[0]);
+                return;
+              } else {
+                response.status(200).send("対象のユーザーはいませんでした。");
+                return;
+              }
+            });
+        }
+      }
+    }
+  }
 });
 
 export default router;
