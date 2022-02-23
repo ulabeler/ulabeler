31fc232749ec0157f2f0a5fd1f5384881b9b7adf
na2na <49822810+na2na-p@users.noreply.github.com>
2022-02-24 03:50:45 +0900

    @userId + 複数キーワードでの検索見通し立った

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-invalid-this */
 import express from "express";
 import { knex } from "../app";
 // import bcrypt from 'bcrypt';
@@ -6,7 +7,7 @@ const router = express.Router();
 // import sideMenuList from "../tools/data/sidemenu.json";
 import { searchWordParse } from "../tools/parser";
 
-import { userTable } from "tools/TypeAlias/tableType_alias";
+import { workTable } from "tools/TypeAlias/tableType_alias";
 
 router.get("/", (request, response) => {
   console.log(request.query.q);
@@ -20,15 +21,33 @@ router.get("/", (request, response) => {
     } else {
       for (let i = 0; i < searchQuery.length; i++) {
         if (searchQuery[i].indexOf("@") !== -1) {
-          knex("user")
-            .select("id", "name", "icon_path", "self_introduction")
-            .where("id", searchQuery[i].slice(1))
-            .then((resultUser: userTable[]) => {
-              if (resultUser.length !== 0) {
-                response.status(200).json(resultUser[0]);
+          // searchQuery[i]以外の要素を別の配列に格納
+          const searchQueryWithoutUserId = searchQuery
+            .slice(0, i)
+            .concat(searchQuery.slice(i + 1));
+          knex("work")
+            .where("created_by_user_id", searchQuery[i].slice(1))
+            // andWhereで検索条件を追加。
+            // searchQueryWithoutUserIdの要素数分だけ繰り返す
+            // nameを検索
+            .andWhere(function () {
+              for (let j = 0; j < searchQueryWithoutUserId.length; j++) {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                this.andWhere(
+                  "name",
+                  "like",
+                  `%${searchQueryWithoutUserId[j]}%`
+                );
+              }
+            })
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .then((resultWork: workTable[]) => {
+              if (resultWork.length !== 0) {
+                response.status(200).json(resultWork);
                 return;
               } else {
-                response.status(200).send("対象のユーザーはいませんでした。");
+                response.status(200).send("対象の作品は見つかりませんでした。");
                 return;
               }
             });
