0b93aa80698bf3c4c568daca971ed10891909226
na2na-p <na2na.rta@gmail.com>
2022-02-24 17:35:39 +0900

    wip

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -88,31 +88,82 @@ router.get("/", (request, response) => {
   console.table(parsedQuery);
   if (parsedQuery.rawQuery.length !== 0) {
     // searchQueryに@が含まれる要素がある場合、userTableからそのユーザーのidを取得
-    if (parsedQuery.rawQuery.length === 1 && parsedQuery.userId) {
-      knex("user")
-        .where("id", parsedQuery.userId)
-        .select("id")
-        .then((user: userTable[]) => {
-          if (user.length === 0) {
-            response.render("search_error", {
-              side_menu: JSON.parse(JSON.stringify(sideMenuList))[
-                `${Boolean(request.user)}`
-              ],
-              recentQuery: request.query.q,
-            });
-            return;
-          } else {
-            response.redirect(
-              `/creator_work/${parsedQuery.userId}?q=${request.query.q}`
-            );
-            return;
-          }
-        });
-    } else if (
-      parsedQuery.other.length === 0 &&
-      parsedQuery.userId === null &&
-      parsedQuery.hashTags
-    ) {
+    if (parsedQuery.userId) {
+      // ユーザーid単体での検索の場合
+      if (parsedQuery.rawQuery.length === 1) {
+        knex("user")
+          .where("id", parsedQuery.userId)
+          .select("id")
+          .then((user: userTable[]) => {
+            if (user.length === 0) {
+              response.render("search_error", {
+                side_menu: JSON.parse(JSON.stringify(sideMenuList))[
+                  `${Boolean(request.user)}`
+                ],
+                recentQuery: request.query.q,
+              });
+              return;
+            } else {
+              response.redirect(
+                `/creator_work/${parsedQuery.userId}?q=${request.query.q}`
+              );
+              return;
+            }
+          })
+          .catch((error: Error) => {
+            console.log(error);
+          });
+      } else if (parsedQuery.hashTags && parsedQuery.other.length === 0) {
+        // userId + ハッシュタグ
+        knex("work")
+          .where("created_by_user_id", parsedQuery.userId)
+          // hashtagはjson型。一つでも一致したらその結果を返す
+          .andWhere(function () {
+            for (let i = 0; i < parsedQuery.hashTags.length; i++) {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              this.andWhere("hashtag", "like", `%${parsedQuery.hashTags[i]}%`);
+            }
+          })
+          .on("query", function (data: string[]) {
+            console.log(data);
+          })
+          .then((resultWork: workTable[]) => {
+            response.status(200).json(resultWork);
+          })
+          .catch((error: Error) => {
+            console.log(error);
+          });
+      } else {
+        // 全部入り
+        knex("work")
+          .where("created_by_user_id", parsedQuery.userId)
+          // hashtagはjson型。一つでも一致したらその結果を返す
+          .andWhere(function () {
+            for (let i = 0; i < parsedQuery.hashTags.length; i++) {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              this.andWhere("hashtag", "like", `%${parsedQuery.hashTags[i]}%`);
+            }
+          })
+          .andWhere(function () {
+            for (let i = 0; i < parsedQuery.other.length; i++) {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              this.andWhere("name", "like", `%${parsedQuery.other[i]}%`);
+            }
+          })
+          .on("query", function (data: string[]) {
+            console.log(data);
+          })
+          .then((resultWork: workTable[]) => {
+            response.status(200).json(resultWork);
+          })
+          .catch((error: Error) => {
+            console.log(error);
+          });
+      }
+    } else if (parsedQuery.hashTags.length !== 0) {
       knex("work")
         // hashtagはjson型。一つでも一致したらその結果を返す
         .where(function () {
