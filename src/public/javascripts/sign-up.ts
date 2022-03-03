74a117eb521094d5e3e7eca60ee76f920da55984
na2na-p <na2na.rta@gmail.com>
2022-03-03 15:27:01 +0900

    一部文言修正

--- src/public/javascripts/sign-up.ts
+++ src/public/javascripts/sign-up.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable max-len */
 // TODO
 // - [x] 変数定義
@@ -7,16 +8,24 @@
 // - [x] ユーザー名にMeCab使った形態素解析で不適切な文字をはじくAPIとかどうですかね
 // - [ ]  エラーメッセージ関係→登録失敗時のエラーページどうする
 // 入力値関係の変数
-const InputUserName = <HTMLInputElement>document.getElementById('input_username'); // <%# 重複可能なユーザー名 %>
-const InputUserId = <HTMLInputElement>document.getElementById('input_userID'); // <%# 重複不可なユーザーID %>
-const InputEmail = <HTMLInputElement>document.getElementById('input_email'); // <%# メールアドレス %>
-const InputEmailConfirm = <HTMLInputElement>document.getElementById('input_email_confirm'); // <%# メールアドレス(確認用) %>
-const InputPassword = <HTMLInputElement>document.getElementById('input_password'); // <%# パスワード %>
-const InputPasswordConfirm = <HTMLInputElement>document.getElementById('input_password_confirm'); // <%# パスワード(確認用) %>
+const InputUserName = <HTMLInputElement>(
+  document.getElementById("input_username")
+); // <%# 重複可能なユーザー名 %>
+const InputUserId = <HTMLInputElement>document.getElementById("input_userID"); // <%# 重複不可なユーザーID %>
+const InputEmail = <HTMLInputElement>document.getElementById("input_email"); // <%# メールアドレス %>
+const InputEmailConfirm = <HTMLInputElement>(
+  document.getElementById("input_email_confirm")
+); // <%# メールアドレス(確認用) %>
+const InputPassword = <HTMLInputElement>(
+  document.getElementById("input_password")
+); // <%# パスワード %>
+const InputPasswordConfirm = <HTMLInputElement>(
+  document.getElementById("input_password_confirm")
+); // <%# パスワード(確認用) %>
 
 // ボタン関係の定義
 // @ts-ignore
-const send = <HTMLButtonElement>document.getElementById('send');
+const send = <HTMLButtonElement>document.getElementById("send");
 let DisableCounterUsername = 0;
 let DisableCounterUserId = 0;
 let DisableCounterEmail = 0;
@@ -27,19 +36,23 @@ let ConfirmRejectCounterEmail = 0;
 let ConfirmRejectCounterPassword = 0;
 
 // エラー関係の表示用変数
-const ErrorUsername = <HTMLInputElement>document.getElementById('error_username'); // <%# ユーザー名エラー %>
-const ErrorUserId = <HTMLInputElement>document.getElementById('error_userID'); // <%# ユーザーIDエラー %>
-const ErrorEmail = <HTMLInputElement>document.getElementById('error_email'); // <%# メールアドレスエラー %>
-const ErrorPassword = <HTMLInputElement>document.getElementById('error_password'); // <%# パスワードエラー %>
+const ErrorUsername = <HTMLInputElement>(
+  document.getElementById("error_username")
+); // <%# ユーザー名エラー %>
+const ErrorUserId = <HTMLInputElement>document.getElementById("error_userID"); // <%# ユーザーIDエラー %>
+const ErrorEmail = <HTMLInputElement>document.getElementById("error_email"); // <%# メールアドレスエラー %>
+const ErrorPassword = <HTMLInputElement>(
+  document.getElementById("error_password")
+); // <%# パスワードエラー %>
 
 // <%#input_usernameは、15文字以内%>
 // <%#入力値を取得して、入力値を検証する%>
-InputUserName.addEventListener('keyup', function() {
+InputUserName.addEventListener("keyup", function () {
   if (InputUserName.value.length > 15) {
-    ErrorUsername.innerText = '15文字以内で入力してください';
+    ErrorUsername.innerText = "15文字以内で入力してください";
     DisableCounterUsername++;
   } else {
-    ErrorUsername.innerText = '';
+    ErrorUsername.innerText = "";
     if (InputUserName.value.length == 0) {
       DisableCounterUsername++;
     } else {
@@ -47,45 +60,47 @@ InputUserName.addEventListener('keyup', function() {
     }
   }
 });
-InputUserName.addEventListener('focusout', function() {
+InputUserName.addEventListener("focusout", function () {
   if (DisableCounterUsername == 0 && InputUserName.value.length > 0) {
     // @ts-ignore
-    axios.post('https://tools.na2na.dev/api/word/is_includeNgWord', {
-      text: InputUserName.value,
-    })
-        .then(function(response: { data: boolean; }) {
-          if (response.data == true) {
-            ErrorUsername.innerText = '不適切なワードが含まれています。';
+    axios
+      .post("https://tools.na2na.dev/api/word/is_includeNgWord", {
+        text: InputUserName.value,
+      })
+      .then(function (response: { data: boolean }) {
+        if (response.data == true) {
+          ErrorUsername.innerText = "不適切なワードが含まれています。";
+          DisableCounterUsername++;
+        } else {
+          ErrorUsername.innerText = "";
+          if (InputUserName.value.length == 0) {
             DisableCounterUsername++;
           } else {
-            ErrorUsername.innerText = '';
-            if (InputUserName.value.length == 0) {
-              DisableCounterUsername++;
-            } else {
-              DisableCounterUsername = 0;
-            }
+            DisableCounterUsername = 0;
           }
-        })
-        .catch(function(error: any) {
-          alert(error); // TODO: エラー時の遷移先
-        });
+        }
+      })
+      .catch(function (error: Error) {
+        alert(error); // TODO: エラー時の遷移先
+      });
   }
 });
 
 // <%#input_userIDは、半角英数字15文字以内%>
 // <%# "_""-""."" "も使用可能とする%>
 // <%#入力値を取得して、入力値を検証する%>
-InputUserId.addEventListener('keyup', function() {
+InputUserId.addEventListener("keyup", function () {
   if (InputUserId.value.length > 15) {
-    ErrorUserId.innerText = '15文字以内で入力してください';
+    ErrorUserId.innerText = "15文字以内で入力してください";
     DisableCounterUserId++;
     return;
   } else if (InputUserId.value.match(/[^a-zA-Z0-9_\-\.]/)) {
-    ErrorUserId.innerText = '使用できる文字は、半角英数字、アンダーバー、ハイフン、ピリオドのみです';
+    ErrorUserId.innerText =
+      "使用できる文字は、半角英数字、アンダーバー、ハイフン、ピリオドのみです";
     DisableCounterUserId++;
     return;
   } else {
-    ErrorUserId.innerText = '';
+    ErrorUserId.innerText = "";
     if (InputUserId.value.length == 0) {
       DisableCounterUserId++;
     } else {
@@ -97,58 +112,61 @@ InputUserId.addEventListener('keyup', function() {
   // 戻り値が"true"なら重複している
   // 戻り値が"false"なら重複していない
 });
-InputUserId.addEventListener('focusout', function() {
+InputUserId.addEventListener("focusout", function () {
   if (DisableCounterUserId == 0) {
     // @ts-ignore
-    axios.post('/api/user/check_userID', {
-      userID: InputUserId.value,
-    })
-        .then(function(response: { data: boolean; }) {
-          if (response.data == true) {
-            ErrorUserId.innerText = '登録済みのユーザーIDです';
+    axios
+      .post("/api/user/check_userID", {
+        userID: InputUserId.value,
+      })
+      .then(function (response: { data: boolean }) {
+        if (response.data == true) {
+          ErrorUserId.innerText = "登録済みのユーザーIDです";
+          DisableCounterUserId++;
+        } else {
+          ErrorUserId.innerText = "";
+          if (InputUserId.value.length == 0) {
             DisableCounterUserId++;
           } else {
-            ErrorUserId.innerText = '';
-            if (InputUserId.value.length == 0) {
-              DisableCounterUserId++;
-            } else {
-              DisableCounterUserId = 0;
-            }
+            DisableCounterUserId = 0;
           }
-        })
-        .catch(function(error: any) {
-          console.log(error);
-        });
+        }
+      })
+      .catch(function (error: Error) {
+        console.log(error);
+      });
   }
 });
 
 // <%#InputEmailは、RFC違反メールアドレスは禁止%>
 // <%#入力値を取得して、入力値を検証する%>
-InputEmail.addEventListener('keyup', function() {
+InputEmail.addEventListener("keyup", function () {
   // @ts-ignore
   if (!mailCheck(InputEmail.value)) {
-    ErrorEmail.innerText = '正しい形式で入力してください。';
+    ErrorEmail.innerText = "正しい形式で入力してください。";
     DisableCounterEmail++;
   } else {
     // @ts-ignore
-    axios.post('/api/user/check_email', {
-      email: InputEmail.value,
-    })
-        .then(function(response: { data: any; }) {
-          console.log(response);
-          if (response.data) {
+    axios
+      .post("/api/user/check_email", {
+        email: InputEmail.value,
+      })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then(function (response: { data: any }) {
+        console.log(response);
+        if (response.data) {
+          DisableCounterEmail++;
+          ErrorEmail.innerText = "このメールアドレスは既に登録されています。";
+          return;
+        } else {
+          ErrorEmail.innerText = "";
+          if (InputEmail.value.length == 0) {
             DisableCounterEmail++;
-            ErrorEmail.innerText = 'このメールアドレスは既に登録されています。';
-            return;
           } else {
-            ErrorEmail.innerText = '';
-            if (InputEmail.value.length == 0) {
-              DisableCounterEmail++;
-            } else {
-              DisableCounterEmail = 0;
-            }
+            DisableCounterEmail = 0;
           }
-        });
+        }
+      });
   }
 });
 
@@ -158,11 +176,11 @@ InputEmail.addEventListener('keyup', function() {
  */
 function checkEmailConfirm() {
   if (InputEmail.value != InputEmailConfirm.value) {
-    ErrorEmail.innerText = 'メールアドレスが一致しません';
+    ErrorEmail.innerText = "メールアドレスが一致しません";
     ConfirmRejectCounterEmail++;
     return false;
   } else {
-    ErrorEmail.innerText = '';
+    ErrorEmail.innerText = "";
     ConfirmRejectCounterEmail = 0;
     if (InputEmailConfirm.value.length == 0) {
       DisableCounterEmail++;
@@ -173,29 +191,29 @@ function checkEmailConfirm() {
   }
 }
 
-
 // <%#input_passwordは、半角英数字8文字以上、20文字以内%>
 // <%# "_""-""."" "も使用可能とする%>
 // <%#大文字小文字それぞれ最低1つ以上を使う事%>
 // <%#入力値を取得して、入力値を検証する%>
-InputPassword.addEventListener('keyup', function() {
+InputPassword.addEventListener("keyup", function () {
   if (InputPassword.value.length < 8) {
-    ErrorPassword.innerText = '8文字以上で入力してください';
+    ErrorPassword.innerText = "8文字以上で入力してください";
     DisableCounterPassword++;
   } else if (InputPassword.value.length > 20) {
-    ErrorPassword.innerText = '20文字以内で入力してください';
+    ErrorPassword.innerText = "20文字以内で入力してください";
     DisableCounterPassword++;
   } else if (InputPassword.value.match(/[^a-zA-Z0-9_\-\.]/)) {
-    ErrorPassword.innerText = '使用できる文字は、半角英数字、アンダーバー、ハイフン、ピリオドのみです';
+    ErrorPassword.innerText =
+      "使用できる文字は、半角英数字、アンダーバー、ハイフン、ピリオドのみです";
     DisableCounterPassword++;
   } else if (!InputPassword.value.match(/[a-z]/)) {
-    ErrorPassword.innerText = '大文字小文字を最低41つ以上含めてください';
+    ErrorPassword.innerText = "大文字小文字を最低1つ以上含めてください";
     DisableCounterPassword++;
   } else if (!InputPassword.value.match(/[A-Z]/)) {
-    ErrorPassword.innerText = '大文字小文字を最低1つ以上含めてください';
+    ErrorPassword.innerText = "大文字小文字を最低1つ以上含めてください";
     DisableCounterPassword++;
   } else {
-    ErrorPassword.innerText = '';
+    ErrorPassword.innerText = "";
     if (InputPassword.value.length == 0) {
       DisableCounterPassword++;
     } else {
@@ -204,19 +222,17 @@ InputPassword.addEventListener('keyup', function() {
   }
 });
 
-
 // 入力した2つのパスワードが合致するかを確認します。
 /**
  * @return {boolean} 入力した2つのパスワードが合致していればtrueを返します。
  */
-// @ts-ignore
 function checkPasswordConfirm() {
   if (InputPassword.value !== InputPasswordConfirm.value) {
-    ErrorPassword.innerText = 'パスワードが一致しません';
+    ErrorPassword.innerText = "パスワードが一致しません";
     ConfirmRejectCounterPassword++;
     return false;
   } else {
-    ErrorPassword.innerText = '';
+    ErrorPassword.innerText = "";
     if (InputEmail.value.length == 0) {
       DisableCounterPassword++;
     } else {
@@ -227,8 +243,14 @@ function checkPasswordConfirm() {
 }
 
 // それぞれのdisable_counterの合計を監視し続ける
-setInterval(function() {
-  if (DisableCounterUsername + DisableCounterUserId + DisableCounterEmail + DisableCounterPassword === 0) {
+setInterval(function () {
+  if (
+    DisableCounterUsername +
+      DisableCounterUserId +
+      DisableCounterEmail +
+      DisableCounterPassword ===
+    0
+  ) {
     send.disabled = false;
   } else {
     send.disabled = true;
@@ -241,32 +263,32 @@ setInterval(function() {
   }
 }, 100);
 
-
 // sendを押した際の動作
 // checkPasswordConfirm,checkEmailConfirmを実行する
-send.addEventListener('click', function() {
+send.addEventListener("click", function () {
   const ValidateEmail = checkEmailConfirm();
   const ValidatePassword = checkPasswordConfirm();
   if (ValidateEmail && ValidatePassword) {
     // @ts-ignore
-    axios.post('/api/user/sign_up', {
-      username: InputUserName.value,
-      userID: InputUserId.value,
-      email: InputEmail.value,
-      password: InputPassword.value,
-    })
-        .then(function(response: { status: number; }) {
+    axios
+      .post("/api/user/sign_up", {
+        username: InputUserName.value,
+        userID: InputUserId.value,
+        email: InputEmail.value,
+        password: InputPassword.value,
+      })
+      .then(function (response: { status: number }) {
         // 201が返ってきた場合は、ユーザー登録が成功したということ
         // それ以外は、エラーが発生したということ
-          if (response.status === 201) {
-            alert('ユーザー登録が完了しました');
-            window.location.href = '/';
-          } else {
-            alert('ユーザー登録に失敗しました');
-          }
-        })
-        .catch(function(error: any) {
-          alert(error); // TODO: エラー時の遷移先
-        });
+        if (response.status === 201) {
+          alert("ユーザー登録が完了しました");
+          window.location.href = "/";
+        } else {
+          alert("ユーザー登録に失敗しました");
+        }
+      })
+      .catch(function (error: Error) {
+        alert(error); // TODO: エラー時の遷移先
+      });
   }
 });
