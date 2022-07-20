a6cec6e276d1f17c80a73fa7d6789073e9e8c4f3
na2na-p <na2na@na2na.dev>
2022-07-20 10:12:59 +0000

    Add next.js(sample)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable require-jsdoc */
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
