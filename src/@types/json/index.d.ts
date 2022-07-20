37bc958f85af616431e5d5dce4d0c373869affd5
na2na-p <na2na@na2na.dev>
2022-07-20 09:20:00 +0000

    v2 initial commit

--- src/@types/json/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*/config.json" {
-  interface CustomConfig {
-    maxViewOnPage: number;
-  }
-
-  const value: CustomConfig;
-  export = value;
-}
--- src/public/javascripts/jquery.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-$(function () {
-  for (let i = 1; i <= 3; i++) {
-    // @ts-ignore
-    $("#box" + i).draggable();
-  }
-});
