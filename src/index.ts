5b2043bc59b7feb38a77791188e6305d5e94e412
na2na-p <na2na@na2na.dev>
2022-07-26 13:02:50 +0900

    Update ミドルウェア追加

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
 import {UlabelerServer} from './server.js';
+import path from 'path';
+import flash from 'connect-flash';
+import favicon from 'serve-favicon';
 
-new UlabelerServer();
+const middleware = [
+	logger('dev'),
+	bodyParser.urlencoded({extended: true, limit: '10mb'}),
+	bodyParser.raw({limit: '10mb'}),
+	bodyParser.json({limit: '10mb'}),
+	express.json(),
+	express.urlencoded({extended: false}),
+	express.static(path.join(path.dirname(new URL(import.meta.url).pathname), '../files/public')),
+	flash(),
+	favicon(path.join(path.dirname(new URL(import.meta.url).pathname), '../files/system', 'favicon.ico')),
+];
+
+new UlabelerServer(middleware);
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, {Router} from 'express';
+import express, {RequestHandler, Router} from 'express';
+import {boundMethod} from 'autobind-decorator';
 import {config} from '@/config.js';
 import {PrismaClient} from '@prisma/client';
 export const prisma = new PrismaClient();
@@ -7,18 +8,25 @@ export class UlabelerServer {
 	private readonly app: express.Application = express();
 	private readonly port: number = config.port;
 
-	constructor(route: Array<Router> = []) {
-		this.app.listen(this.port, () => {
-			console.log(`Server listening on port ${this.port}`);
-		});
+	constructor(middleware: Array<RequestHandler>, route: Array<Router> = []) {
 		// ミドルウェアを追加
-		this.app.use(express.json());
-		this.app.use(express.urlencoded({extended: true}));
-		this.app.use(express.static('public'));
+		this.setMiddleware(middleware);
 
 		// ルーティング追加
 		route.forEach((r) => {
 			// this.app.use(r.path, r.router);
 		});
+
+		// 起動
+		this.app.listen(this.port, () => {
+			console.log(`Server listening on port ${this.port}`);
+		});
 	};
+
+	@boundMethod
+	private setMiddleware(middleware: Array<RequestHandler>): void {
+		middleware.forEach((m) => {
+			this.app.use(m);
+		});
+	}
 };
