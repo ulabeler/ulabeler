4010b3e99b15adaff6227b71b8ec3899e38659ee
na2na-p <na2na@na2na.dev>
2022-07-28 10:48:17 +0000

    Initial Commit

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import {UlabelerServer} from './server.js';
-import path from 'path';
+// import path from 'path';
 import flash from 'connect-flash';
-import favicon from 'serve-favicon';
-import {dirname} from './utils/dirname.js';
+// import favicon from 'serve-favicon';
+// import {dirname} from './utils/dirname.js';
 // import {Ulabeler} from './types/utils.js';
 
 // ミドルウェアを配列の形で列挙
@@ -16,9 +16,9 @@ const middleware: Array<Function> | Array<string> = [
 	bodyParser.json({limit: '10mb'}),
 	express.json(),
 	express.urlencoded({extended: false}),
-	express.static(path.join(dirname(import.meta.url), '../files/public')),
+	// express.static(path.join(dirname(import.meta.url), '../files/public')),
 	flash(),
-	favicon(path.join(dirname(import.meta.url), '../files/system', 'favicon.ico')),
+	// favicon(path.join(dirname(import.meta.url), '../files/system', 'favicon.ico')),
 ];
 
 // ルーティングを配列の形で列挙
